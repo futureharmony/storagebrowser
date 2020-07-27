crystaluniverse/crystaldrive-ui

feat: add param to disable img resizing

http/static.go
@@ -39,6 +39,7 @@ func handleWithStaticData(w http.ResponseWriter, _ *http.Request, d *data, box *
 		"CSS":             false,
 		"ReCaptcha":       false,
 		"Theme":           d.settings.Branding.Theme,
+		"EnableThumbs":    d.server.ResizeImg,
 	}
 
 	if d.settings.Branding.Files != "" {

settings/settings.go
@@ -30,14 +30,15 @@ func (s *Settings) GetRules() []rules.Rule {
 
 // Server specific settings.
 type Server struct {
-	Root    string `json:"root"`
-	BaseURL string `json:"baseURL"`
-	Socket  string `json:"socket"`
-	TLSKey  string `json:"tlsKey"`
-	TLSCert string `json:"tlsCert"`
-	Port    string `json:"port"`
-	Address string `json:"address"`
-	Log     string `json:"log"`
+	Root      string `json:"root"`
+	BaseURL   string `json:"baseURL"`
+	Socket    string `json:"socket"`
+	TLSKey    string `json:"tlsKey"`
+	TLSCert   string `json:"tlsCert"`
+	Port      string `json:"port"`
+	Address   string `json:"address"`
+	Log       string `json:"log"`
+	ResizeImg bool   `json:"resizeImg"`
 }
 
 // Clean cleans any variables that might need cleaning.

futureharmony/storagebrowser

http/static.go
@@ -39,6 +39,7 @@ func handleWithStaticData(w http.ResponseWriter, _ *http.Request, d *data, box *
 		"CSS":             false,
 		"ReCaptcha":       false,
 		"Theme":           d.settings.Branding.Theme,
+		"EnableThumbs":    d.server.EnableThumbnails,
 	}
 
 	if d.settings.Branding.Files != "" {

settings/settings.go
@@ -30,14 +30,16 @@ func (s *Settings) GetRules() []rules.Rule {
 
 // Server specific settings.
 type Server struct {
-	Root    string `json:"root"`
-	BaseURL string `json:"baseURL"`
-	Socket  string `json:"socket"`
-	TLSKey  string `json:"tlsKey"`
-	TLSCert string `json:"tlsCert"`
-	Port    string `json:"port"`
-	Address string `json:"address"`
-	Log     string `json:"log"`
+	Root             string `json:"root"`
+	BaseURL          string `json:"baseURL"`
+	Socket           string `json:"socket"`
+	TLSKey           string `json:"tlsKey"`
+	TLSCert          string `json:"tlsCert"`
+	Port             string `json:"port"`
+	Address          string `json:"address"`
+	Log              string `json:"log"`
+	EnableThumbnails bool   `json:"enableThumbnails"`
+	ResizePreview    bool   `json:"resizePreview"`
 }
 
 // Clean cleans any variables that might need cleaning.